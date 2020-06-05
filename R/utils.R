cb554173a2cdcb010a4001a83a1877857bc3875d
Thomas Shafee <TS404@users.noreply.github.com>
2020-06-05 14:17:23 +1000

    Import utils from bearloga/WikidataQueryServiceR
    Import from https://github.com/bearloga/WikidataQueryServiceR/blob/master/R/utils.R

--- R/utils.R
+++ R/utils.R
@@ -193,7 +193,7 @@ get_names_from_properties <- function(properties){
   return(property_names)
 }
 
-
+# Format short form person names (first initial and surname, or just initials)
 initials <- function(x,type="FLast"){
   if (type=="FLast"){
     gsub("^([A-Za-z]).* ([A-Za-z]*)", "\\1 \\2", x)
@@ -202,6 +202,7 @@ initials <- function(x,type="FLast"){
   }
 }
 
+# Remove special characters (these can mess up wikidata read-writes)
 unspecial <- function(x){
   out <- x
   for(i in 1:ncol(x)){
@@ -213,4 +214,43 @@ unspecial <- function(x){
     } 
   }
   return(as_tibble(out))
+}
+
+#' @title Get an example SPARQL query from Wikidata
+#' @description Gets the specified example(s) from
+#'   [SPARQL query service examples page](https://www.wikidata.org/wiki/Wikidata:SPARQL_query_service/queries/examples)
+#'   using [Wikidata's MediaWiki API](https://www.wikidata.org/w/api.php).
+#' @details If you are planning on extracting multiple examples, please provide
+#'   all the names as a single vector for efficiency.
+#' @param example_name the names of the examples as they appear on
+#'   [this page](https://www.wikidata.org/wiki/Wikidata:SPARQL_query_service/queries/examples)
+#' @return The SPARQL query as a character vector.
+#' @examples
+#' \dontrun{
+#' sparql_query <- extract_example(c("Cats", "Horses"))
+#' query_wikidata(sparql_query)
+#' # returns a named list with two data frames
+#' # one called "Cats" and one called "Horses"
+#'
+#' sparql_query <- extract_example("Largest cities with female mayor")
+#' cat(sparql_query)
+#' query_wikidata(sparql_query)
+#' }
+#' @seealso [query_wikidata]
+#' @export
+get_example <- function(example_name) {
+  content <- WikipediR::page_content(
+    domain = "www.wikidata.org",
+    page_name = "Wikidata:SPARQL query service/queries/examples",
+    as_wikitext = TRUE
+  )
+  wiki <- strsplit(content$parse$wikitext$`*`, "\n")[[1]]
+  wiki <- wiki[wiki != ""]
+  return(vapply(example_name, function(example_name) {
+    heading_line <- which(grepl(paste0("^===\\s?", example_name, "\\s?===$"), wiki, fixed = FALSE))
+    start_line <- which(grepl("{{SPARQL", wiki[(heading_line + 1):length(wiki)], fixed = TRUE))[1]
+    end_line <- which(grepl("}}", wiki[(heading_line + start_line + 1):length(wiki)], fixed = TRUE))[1]
+    query <- paste0(wiki[(heading_line + start_line):(heading_line + start_line + end_line - 1)], collapse = "\n")
+    return(sub("^\\s*\\{\\{SPARQL2?\\n?\\|query\\=", "", query))
+  }, ""))
 }
