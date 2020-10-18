c6c30d0f1b73e229b3f654d54704e43be130786d
Thomas Shafee <TS404@users.noreply.github.com>
2020-10-18 15:27:40 +1100

    adding roxygen descriptions

--- R/utils.R
+++ R/utils.R
@@ -1,16 +1,36 @@
-#Generic queryin' function for direct Wikidata calls. Wraps around WikipediR::page_content.
+# -------- Gets --------
+
+#Generic queryin' function for direct Wikidata calls. Wraps around WikipediR::page_content. - Ironholds
+#'@title Download a Wikidata item
+#'@description Utility wrapper for wikidata API to download item. Used by \code{get_item} and \code{get_property}
+#'
+#'@param title the wikidata item or property as a string
+#'
+#'@return a download of the full wikidata object (item or property) formatted as a nested json list
+#'
+#'@export
 wd_query <- function(title, ...){
   result <- WikipediR::page_content(domain = "wikidata.org", page_name = title, as_wikitext = TRUE,
-                                    httr::user_agent("WikidataR - https://github.com/Ironholds/WikidataR"),
+                                    httr::user_agent("WikidataR - https://github.com/TS404/WikidataR"),
                                     ...)
   output <- jsonlite::fromJSON(result$parse$wikitext[[1]])
   return(output)
 }
 
-#Query for a random item in "namespace" (ns). Essentially a wrapper around WikipediR::random_page.
+# Query for a random item in "namespace" (ns). Essentially a wrapper around WikipediR::random_page. - Ironholds
+#'@title Download random Wikidata items
+#'@description Utility wrapper for wikidata API to download random items. Used by \code{random_item}
+#'
+#'@param ns string indicating namespace, most commonly "Main" for QID items, "Property" for PID properties
+#'
+#'@param limit how many random objesct to return
+#'
+#'@return a download of the full wikidata objects (items or properties) formatted as nested json lists
+#'
+#'@export
 wd_rand_query <- function(ns, limit, ...){
   result <- WikipediR::random_page(domain = "wikidata.org", as_wikitext = TRUE, namespaces = ns,
-                                   httr::user_agent("WikidataR - https://github.com/Ironholds/WikidataR"),
+                                   httr::user_agent("WikidataR - https://github.com/TS404/WikidataR"),
                                    limit = limit, ...)
   output <- lapply(result, function(x){jsonlite::fromJSON(x$wikitext[[1]])})
   class(output) <- "wikidata"
@@ -18,18 +38,26 @@ wd_rand_query <- function(ns, limit, ...){
   
 }
 
-#Generic input checker. Needs additional stuff for property-based querying
-#because namespaces are weird, yo.
-check_input <- function(input, substitution){
-  in_fit <- grepl("^\\d+$",input)
-  if(any(in_fit)){
-    input[in_fit] <- paste0(substitution, input[in_fit])
-  }
-  return(input)
-}
-
 #Generic, direct access to Wikidata's search functionality.
-searcher <- function(search_term, language, limit, type, ...){
+#'@title Convert an input to a item QID
+#'@description Convert an input string to the most likely item QID
+#'
+#'@param x a vector of strings representaing wikidata items
+#'
+#'@return if the inputted string is a valid QID, return the string.
+#'If the inputted string matches an item label, return its QID.
+#'If the inputted string matches multiple labels of multiple items, return the QID of the first hit.
+#'
+#'@examples
+#'# if input string is a valid QID
+#'as_qid("Q42")
+#'# if input string matches multiple item labels
+#'as_qid("Douglas Adams")
+#'# if input string matches a single unique label
+#'as_qid("Douglas Adams and the question of arterial blood pressure in mammals")
+#'
+#'@export
+search <- function(search_term, language, limit, type, ...){
   result <- WikipediR::query(url = "https://www.wikidata.org/w/api.php", out_class = "list", clean_response = FALSE,
                              query_param = list(
                                action   = "wbsearchentities", 
@@ -43,153 +71,14 @@ searcher <- function(search_term, language, limit, type, ...){
   return(result)
 }
 
-sparql_query <- function(params, ...){
-  result <- httr::GET("https://query.wikidata.org/bigdata/namespace/wdq/sparql",
-                      query = list(query = params),
-                      httr::user_agent("WikidataR - https://github.com/Ironholds/WikidataR"),
-                      ...)
-  httr::stop_for_status(result)
-  return(httr::content(result, as = "parsed", type = "application/json"))
-}
-
-# Simple tests of strings for whether they adhere to common wikidata formats 
-is.qid  <- function(x){grepl("^[Qq][0-9]+$",x)}
-is.pid  <- function(x){grepl("^[Pp][0-9]+$",x)}
-is.sid  <- function(x){grepl("^[Ss][0-9]+$",x)}
-is.date <- function(x){grepl("[0-9]{1,4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}",x)}
-is.quot <- function(x){grepl("^\".+\"$",x)}
-is.coord <- function(x){grepl("@-?([1-8]?\\d(\\.\\d+)?|90(\\.0+)?)/-?(180(\\.0+)?|((1[0-7]\\d)|([1-9]?\\d))(\\.\\d+)?)$",x)}
-is.empty<- function(x){x==""}
-
-# Simple functions to convert plain text descriptions into their most likely QID/PIDs
-as_qid <- function(x){if(all(is.qid(x))){x}else{WikidataR::find_item(x)[[1]]$id}}
-as_pid <- function(x){if(all(is.pid(x))){x}else{WikidataR::find_property(x)[[1]]$id}}
-as_sid <- function(x){if(all(is.sid(x))){x}
-  else if(all(is.pid(x))){gsub("P","S",x,ignore.case = 1)}
-  else{gsub("P","S",WikidataR::find_property(x)[[1]]$id)}
-  }
-
-#'@title Extract Claims from Returned Item Data
-#'@description extract claim information from data returned using
-#'\code{\link{get_item}}.
-#'
-#'@param items a list of one or more Wikidata items returned with
-#'\code{\link{get_item}}.
-#'
-#'@param claims a vector of claims (in the form "P321", "P12") to look for
-#'and extract.
-#'
-#'@return a list containing one sub-list for each entry in \code{items},
-#'and (below that) the found data for each claim. In the event a claim
-#'cannot be found for an item, an \code{NA} will be returned
-#'instead.
-#'
-#'@examples
-#'# Get item data
-#'adams_data <- get_item("42")
-#'
-#'# Get claim data
-#'claims <- extract_claims(adams_data, "P31")
-#'
-#'@export
-extract_claims <- function (items,
-                            claims){
-  claims <- sapply(claims,as_pid)
-  output <- lapply(items, function(x, claims) {
-    return(lapply(claims, function(claim, obj) {
-      which_match <- which(names(obj$claims) == claim)
-      if (!length(which_match)) {
-        return(NA)
-      }
-      return(obj$claims[[which_match[1]]])
-    }, obj = x))
-  }, claims = claims)
-  return(output)
-}
-
-qid_from_DOI <- function(DOI = '10.15347/WJM/2019.001'){
-  qid_from_DOI_nest1 <- function(x){paste('SELECT ?DOI WHERE {?DOI wdt:P356 "',
-                                          x,
-                                          '"}',
-                                          sep='')}
-  qid_from_DOI_nest2 <- function(x){tail(stringr::str_split(x,pattern = "/")[[1]],n=1)}
-  sparql_query <- lapply(DOI,qid_from_DOI_nest1)
-  article.qr   <- lapply(query_wikidata(sparql_query),as_tibble)
-  names(article.qr)<-DOI
-  article.qid  <- unlist(lapply(article.qr,qid_from_DOI_nest2))
-  return(article.qid)
-}
-
-qid_from_name <- function(name  = 'Thomas Shafee',
-                          limit = 100){
-  qid_from_name_nest1 <- function(x){lapply(x,"[[","id")}
-  item.qs  <- lapply(name,find_item, limit=limit)
-  item.qid <- lapply(item.qs,qid_from_name_nest1)
-  names(item.qid) <- name
-  item.qid <- unlist(item.qid)
-  return(item.qid)
-}
-
-list_properties <- function (item,
-                             names=FALSE){
-  properties.p <- lapply(lapply(item,"[[","claims"),names)
-  if(names){
-    if(length(item)==1){
-      names(properties.p) <- unlist(lapply(lapply(lapply(get_property(properties.p),"[[","labels"),"[[","en"),"[[","value"))
-    }
-  }
-  return(properties.p)
-}
-
-get_names_from_properties <- function(properties){
-  get_names_from_properties_nest1 <- function(x){
-    out <- lapply(lapply(lapply(lapply(x,"[[","mainsnak"),"[[","datavalue"),"[[","value"),"[[","id")
-    names(out) <- lapply(lapply(lapply(x,"[[","mainsnak"),"[[","property"),"[[",1)
-    return(out)
-  }
-  get_names_from_properties_nest2 <- function(x){
-    out <- lapply(x,get_item)
-    return(out)
-  }
-  get_names_from_properties_nest3.1 <- function(x){
-    out <- lapply(lapply(lapply(x,"[[","labels"),"[[","en"),"[[","value")
-    names(out) <- lapply(x,"[[","id")
-    return(out)
-  }
-  get_names_from_properties_nest3 <- function(x){
-    out <- lapply(x,get_names_from_properties_nest3.1)
-    return(out)
-  }
-  
-  property_values.qid <- lapply(properties,get_names_from_properties_nest1)
-  property_values.q   <- lapply(property_values.qid,get_names_from_properties_nest2)
-  property_names      <- lapply(property_values.q, get_names_from_properties_nest3)
-  property_names      <- lapply(lapply(property_names,unlist),enframe,name = "QID") 
-  return(property_names)
-}
-
-# Format short form person names (first initial and surname, or just initials)
-initials <- function(x,type="FLast"){
-  if (type=="FLast"){
-    gsub("^([A-Za-z]).* ([A-Za-z]*)", "\\1 \\2", x)
-  }else{
-    gsub("(.)\\S* *", "\\1", x)
-  }
-}
-
-# Remove special characters (these can mess up wikidata read-writes)
-unspecial <- function(x){
-  out <- x
-  for(i in 1:ncol(x)){
-    out[[i]] <- iconv(x[[i]],to = 'ASCII//TRANSLIT')
-    if(Hmisc::all.is.numeric(x[[i]])){
-      out[[i]] <- as.numeric(out[[i]])
-    }else{
-      out[[i]] <- as.factor(out[[i]])
-    } 
-  }
-  return(as_tibble(out))
-}
+# sparql_query <- function(params, ...){
+#   result <- httr::GET("https://query.wikidata.org/bigdata/namespace/wdq/sparql",
+#                       query = list(query = params),
+#                       httr::user_agent("WikidataR - https://github.com/Ironholds/WikidataR"),
+#                       ...)
+#   httr::stop_for_status(result)
+#   return(httr::content(result, as = "parsed", type = "application/json"))
+# }
 
 #' @title Get an example SPARQL query from Wikidata
 #' @description Gets the specified example(s) from
@@ -230,9 +119,268 @@ get_example <- function(example_name) {
   }, ""))
 }
 
+# -------- Format checkers --------
+# Simple tests of strings for whether they adhere to common wikidata formats
+is.qid   <- function(x){grepl("^[Qq][0-9]+$",x)}
+is.pid   <- function(x){grepl("^[Pp][0-9]+$",x)}
+is.sid   <- function(x){grepl("^[Ss][0-9]+$",x)}
+is.date  <- function(x){grepl("[0-9]{1,4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}",x)}
+is.quot  <- function(x){grepl("^\".+\"$",x)}
+is.coord <- function(x){grepl("@-?([1-8]?\\d(\\.\\d+)?|90(\\.0+)?)/-?(180(\\.0+)?|((1[0-7]\\d)|([1-9]?\\d))(\\.\\d+)?)$",x)}
+is.empty <- function(x){x==""}
 
+#Generic input checker. Needs additional stuff for property-based querying
+#because namespaces are weird, yo. - Ironholds
+#'@title Generic input checker
+#'@description Utility function to handle namespaces. Used by \code{get_item} and \code{get_property}
+#'
+#'@param input string to check
+#'
+#'@param substitution string for what's been looked for
+#'
+#'@return boolian indicating whether the checked string contains a match for the substitution string 
+#'
+#'@export
+check_input <- function(input, substitution){
+  in_fit <- grepl("^\\d+$",input)
+  if(any(in_fit)){
+    input[in_fit] <- paste0(substitution, input[in_fit])
+  }
+  return(input)
+}
+# -------- Converters --------
+# Simple functions to convert plain text descriptions into their most likely QID/PIDs
+
+#'@title Convert an input to a item QID
+#'@description Convert an input string to the most likely item QID
+#'
+#'@param x a vector of strings representaing wikidata items
+#'
+#'@return if the inputted string is a valid QID, return the string.
+#'If the inputted string matches an item label, return its QID.
+#'If the inputted string matches multiple labels of multiple items, return the QID of the first hit.
+#'
+#'@examples
+#'# if input string is a valid QID
+#'as_qid("Q42")
+#'# if input string matches multiple item labels
+#'as_qid("Douglas Adams")
+#'# if input string matches a single unique label
+#'as_qid("Douglas Adams and the question of arterial blood pressure in mammals")
+#'
+#'@export
+as_qid <- function(x){if(all(is.qid(x))){x}else{WikidataR::find_item(x)[[1]]$id}}
+
+#'@title Convert an input to a property PID
+#'@description Convert an input string to the most likely property PID
+#'
+#'@param x a vector of strings representaing wikidata properties
+#'
+#'@return if the inputted string is a valid PID, return the string.
+#'If the inputted string matches a property label, return its PID.
+#'If the inputted string matches multiple labels of multiple properties, return the PID of the first hit.
+#'
+#'@examples
+#'# if input string is a valid PID
+#'as_pid("P50")
+#'# if input string matches multiple item labels
+#'as_pid("author")
+#'# if input string matches a single unique label
+#'as_pid("Scopus author ID")
+#'
+#'@export
+as_pid <- function(x){if(all(is.pid(x))){x}else{WikidataR::find_property(x)[[1]]$id}}
+
+#'@title Convert an input to a source property SID
+#'@description Convert an input string to the most likely source SID (equivalent to PID)
+#'
+#'@param x a vector of strings representaing wikidata source properties
+#'
+#'@return if the inputted string is a valid SID, return the string.
+#'If the inputted string matches a property label, return its SID
+#'If the inputted string matches multiple labels of multiple properties, return the SID of the first hit.
+#'
+#'@examples
+#'# if input string is a valid SID
+#'as_pid("S854")
+#'# if input string matches multiple item labels
+#'as_pid("URL")
+#'# if input string matches a single unique label
+#'as_pid("Reference URL")
+#'
+#'@export
+as_sid <- function(x){if(all(is.sid(x))){x}
+  else if(all(is.pid(x))){gsub("P","S",x,ignore.case = 1)}
+  else{gsub("P","S",WikidataR::find_property(x)[[1]]$id)}
+  }
+
+# -------- Wikidata object manipulation --------
+#'@title Extract Claims from Returned Item Data
+#'@description extract claim information from data returned using
+#'\code{\link{get_item}}.
+#'
+#'@param items a list of one or more Wikidata items returned with
+#'\code{\link{get_item}}.
+#'
+#'@param claims a vector of claims (in the form "P321", "P12") to look for
+#'and extract.
+#'
+#'@return a list containing one sub-list for each entry in \code{items},
+#'and (below that) the found data for each claim. In the event a claim
+#'cannot be found for an item, an \code{NA} will be returned
+#'instead.
+#'
+#'@examples
+#'# Get item data
+#'adams_data <- get_item("42")
+#'
+#'# Get claim data
+#'claims <- extract_claims(adams_data, "P31")
+#'
+#'@export
+extract_claims <- function (items,
+                            claims){
+  claims <- sapply(claims,as_pid)
+  output <- lapply(items, function(x, claims) {
+    return(lapply(claims, function(claim, obj) {
+      which_match <- which(names(obj$claims) == claim)
+      if (!length(which_match)) {
+        return(NA)
+      }
+      return(obj$claims[[which_match[1]]])
+    }, obj = x))
+  }, claims = claims)
+  return(output)
+}
+
+#'@title List properties of a Wikidata item
+#'@description for a downloaded wikidata item, list the properties of all statements
+#'
+#'@param items a list of one or more Wikidata items returned with
+#'\code{\link{get_item}}.
+#'
+#'@param claims a vector of claims (in the form "P321", "P12") to look for
+#'and extract.
+#'
+#'@return a list containing one sub-list for each entry in \code{items},
+#'and (below that) the found data for each claim. In the event a claim
+#'cannot be found for an item, an \code{NA} will be returned
+#'instead.
+#'
+#'@examples
+#'# Get item data
+#'adams_data <- get_item("42")
+#'
+#'# Get claim data
+#'claims <- extract_claims(adams_data, "P31")
+#'
+#'@export
+list_properties <- function (item,
+                             names=FALSE){
+  properties.p <- lapply(lapply(item,"[[","claims"),names)
+  if(names){
+    if(length(item)==1){
+      names(properties.p) <- unlist(lapply(lapply(lapply(get_property(properties.p),"[[","labels"),"[[","en"),"[[","value"))
+    }
+  }
+  return(properties.p)
+}
+
+#Note: This one isn't very well named. not really the property names, more the predicate names
+
+#'@title Get names of properties
+#'@description For a clam or set of claims, return the names of the properties  
+#'
+#'@param properties a claims list from \code{extract_claims}
+#'
+#'@return tibble of labels for each property for a set of claims
+#'
+#'@export
+get_names_from_properties <- function(properties){
+  get_names_from_properties_nest1 <- function(x){
+    out <- lapply(lapply(lapply(lapply(x,"[[","mainsnak"),"[[","datavalue"),"[[","value"),"[[","id")
+    names(out) <- lapply(lapply(lapply(x,"[[","mainsnak"),"[[","property"),"[[",1)
+    return(out)
+  }
+  get_names_from_properties_nest2 <- function(x){
+    out <- lapply(x,get_item)
+    return(out)
+  }
+  get_names_from_properties_nest3.1 <- function(x){
+    out <- lapply(lapply(lapply(x,"[[","labels"),"[[","en"),"[[","value")
+    names(out) <- lapply(x,"[[","id")
+    return(out)
+  }
+  get_names_from_properties_nest3 <- function(x){
+    out <- lapply(x,get_names_from_properties_nest3.1)
+    return(out)
+  }
+  
+  property_values.qid <- lapply(properties,get_names_from_properties_nest1)
+  property_values.q   <- lapply(property_values.qid,get_names_from_properties_nest2)
+  property_names      <- lapply(property_values.q, get_names_from_properties_nest3)
+  property_names      <- lapply(lapply(property_names,unlist),enframe,name = "QID") 
+  return(property_names)
+}
+
+# -------- Misc. string manipulation --------
+#'@title Format short form person names
+#'@description Converting names into first initial and surname, or just initials
+#'
+#'@param x a vector of people's names as strings
+#'
+#'@param format a vector of strings of either "FLast" or "FL" to indicate the output format
+#'
+#'@return the inputted name strings with first names shortened based on the
+#'selected format.
+#'
+#'@export
+initials <- function(x,format="FLast"){
+  if (format=="FLast"){
+    gsub("^([A-Za-z]).* ([A-Za-z]*)", "\\1 \\2", x)
+  }else{
+    gsub("(.)\\S* *", "\\1", x)
+  }
+}
+
+#'@title Remove special characters
+#'@description Special characters can otherwise mess up wikidata read-writes
+#'
+#'@param x a vector of strings to check for special characters
+#'
+#'@return the inputted strings with special characters replaced with
+#'closest match plan characters.
+#'
+#'@export
+unspecial <- function(x){
+  out <- x
+  for(i in 1:ncol(x)){
+    out[[i]] <- iconv(x[[i]],to = 'ASCII//TRANSLIT')
+    if(Hmisc::all.is.numeric(x[[i]])){
+      out[[i]] <- as.numeric(out[[i]])
+    }else{
+      out[[i]] <- as.factor(out[[i]])
+    } 
+  }
+  return(as_tibble(out))
+}
+
+#'@title Extract a paragraph of text
+#'@description Return the nth paragraph of a section of text. 
+#'Useful for extracting information from wikipedia or other wikimarkup text
+#'
+#'@param text the input text as a string
+#'
+#'@param para number indicating whichparagraph(s) to return (default=1)
+#'
+#'@param templ an optional string specifying a mediawikitemplate within
+#'which to restrict the search restrict search 
+#'
+#'@return the nth paragraph of the input text.
+#'
+#'@export
 extract_para <- function(text,
-                         para,
+                         para=1,
                          templ=NULL){
   extract_para_nest1 <- function(x,y){
     out <- lapply(x,gsub,pattern=".*= *| *\\|",replacement="")
