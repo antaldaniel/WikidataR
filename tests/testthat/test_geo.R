853d6008a6fc5c52f6ceb4508999370164f95ca9
Thomas Shafee <TS404@users.noreply.github.com>
2021-05-06 14:47:09 +1000

    corrections to test for updated formats

--- tests/testthat/test_geo.R
+++ tests/testthat/test_geo.R
@@ -11,16 +11,16 @@ testthat::test_that("Simple entity-based geo lookups work", {
 testthat::test_that("Language-variant entity-based geo lookups work", {
   field_names <- c("item", "name", "latitutde", "longitude", "entity")
   sf_locations <- get_geo_entity("Q62", language = "fr")
-  testthat::expect_true(is.data.frame(sf_locations))
-  testthat::expect_true(all(field_names == names(sf_locations)))
-  testthat::expect_true(unique(sf_locations$entity) == "Q62")
+  testthat::expect_true((is.data.frame(sf_locations))|(is.list(sf_locations)))
+  testthat::expect_true(all(field_names == colnames(sf_locations)))
+  testthat::expect_true(unique(sf_locations$entity) == "Q62") 
 })
 
 testthat::test_that("Radius restricted entity-based geo lookups work", {
   field_names <- c("item", "name", "latitutde", "longitude", "entity")
   sf_locations <- get_geo_entity("Q62", radius = 1)
   testthat::expect_true(is.data.frame(sf_locations))
-  testthat::expect_true(all(field_names == names(sf_locations)))
+  testthat::expect_true(all(field_names == colnames(sf_locations)))
   testthat::expect_true(unique(sf_locations$entity) == "Q62")
 })
 
